9e355899bb3dd947d0e9d57bafe2a894f6175678
Naa-abia <augusta.addy@meltwater.org>
2022-10-26 10:13:32 +0000

    added methods and explained return types

--- Class work/java/Dog.java
+++ Class work/java/Dog.java
@@ -3,13 +3,25 @@ public class Dog {
     String name = "";
     int age = 0;
 
-   
-   //METHODS AND RETURNS
-    static void bark(){ //this is a static that returns nothing 
+    // METHODS AND RETURNS
+
+    //METHODS
+    static void bark() { // this is a static that returns nothing
         System.out.println("Barking");
     }
 
-    void run(){ //this is a dynamic method that returns nothing 
+    void run() { // this is a dynamic method that returns nothing
         System.out.println("Running");
+    }
+
+
+    //RETURN TYPES
+    String getName() { // when you decide to a string there whatever it returns must be a keyword which
+                       // is a string . Void will have to go out now .
+        return name;
+    }
+
+    int getDogAge(){
+        return age;
     }
 }
