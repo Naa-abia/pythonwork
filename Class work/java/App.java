4c4ce224c4321e83ee101b71068e8bef77b9e971
Naa-abia <augusta.addy@meltwater.org>
2022-10-26 10:04:27 +0000

    adding dynamic methods and static methods to Dog()

---
+++ Class work/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    
+    public static void main(String[] args) {
+      
+        //calling the method from another class which has that same method .
+        Dog bruce = new Dog();
+        bruce.run();
+    }
+}
---
+++ Class work/java/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+
+    String name = "";
+    int age = 0;
+
+   
+   //METHODS AND RETURNS
+    static void bark(){ //this is a static that returns nothing 
+        System.out.println("Barking");
+    }
+
+    void run(){ //this is a dynamic method that returns nothing 
+        System.out.println("Running");
+    }
+}
