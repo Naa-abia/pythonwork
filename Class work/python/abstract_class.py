c91a6dcce0206a5e60c89f0c4b00723f04587856
Naa-abia <augusta.addy@meltwater.org>
2022-10-31 10:08:08 +0000

    moving abstract class too

--- Class work/python/abstract_class.py
+++ Class work/python/abstract_class.py
@@ -1,9 +1,10 @@
 # Abstraction is the removal of complexities from a data to make it more user friendly 
-# # Abstract class cannot be instantiated
+# Abstract class cannot be instantiated
 # Abstract classes can only be accessed via inheritance
 
 
 from abc import ABC, abstractmethod
+from ast import Pass
 # abstractmethod is a decorator used to add meta data to the method class
 
 
@@ -21,6 +22,11 @@ class UserAbstract(ABC):
     def get_all_users(self):
         pass
 
+    @abstractmethod
+    def get_user_by_id(self):
+        pass
+
+
 class UserManager(UserAbstract):
     def create_user(self, user: User):
        print("user information")
@@ -28,3 +34,11 @@ class UserManager(UserAbstract):
 
     def get_all_users(self):
         print("Hello Abia ! we are getting all your users")
+
+    def get_user_by_id(self, userid):
+        print("hello world")
+
+    
+# instatiating the UserManager class
+user_manager = UserManager()
+user_manager.get_all_users()
